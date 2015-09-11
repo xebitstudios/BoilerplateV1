6686e70b0fc1a33d4903aed2341ff97e298359e9
unknown <aoduyela@EIT-TECH-32BNJ0.ebsco.com>
2015-09-11 09:01:15 -0500

    added files

---
+++ app.js
@@ -0,0 +1,58 @@
+﻿var express = require('express');
+var bodyParser = require('body-parser');
+var _ = require('underscore');
+var http = require('http');
+
+var app = express();
+// app.use(express.favicon());
+// app.use(express.logger('dev'));
+// app.use(express.json());
+// app.use(express.urlencoded());
+// app.use(express.methodOverride());
+
+var static = require('./lib/static.js').map;
+
+var routes = require('./routes/index');
+app.use('/', routes);
+// app.get('/about', routes);
+// app.get('/contact', routes);
+
+var handlebars = require('express3-handlebars')
+		.create({
+    defaultLayout: 'main',
+    helpers: {
+        static: function (name) {
+            return require('./lib/static.js').map(name);
+        },
+        section: function (name, options) {
+            if (!this._sections) this._sections = {};
+            this._sections[name] = options.fn(this);
+            return null;
+        }
+    }
+});
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', process.env.PORT || 3000);
+
+app.use(express.static(__dirname + '/public'));
+
+// app.use('/', routes.index);
+// app.get('/about', routes.about);
+// app.get('/contact', routes.contact);
+
+app.use(function (req, res, next) {
+    res.status(404);
+    res.render('404');
+});
+
+app.use(function (err, req, res, next) {
+    console.error(err.stack);
+    res.status(500);
+    res.render('500');
+});
+
+app.listen(app.get('port'), function () {
+    console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
---
+++ routes/index.js
@@ -0,0 +1,18 @@
+﻿var express = require('express');
+var bodyParser = require('body-parser');
+var _ = require('underscore');
+var router = express.Router();
+
+router.get('/', function (req, res) {
+    res.render('home', { layout: 'main' });
+});
+
+router.get('/about', function (req, res) {
+    res.render('about', { layout: 'main' });
+});
+
+router.get('/contact', function (req, res) {
+    res.render('contact', { layout: 'main' });
+});
+
+module.exports = router;
